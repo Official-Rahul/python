10f028e32c18bcdcc32ee19f49ec5e855f655a27
Official-Rahul <official.rahul.email@gmail.com>
2025-04-07 00:30:17 +0530

    circle and rectangle area

---
+++ circleArea.py
@@ -0,0 +1,3 @@
+r=int(input("Radius of Circle: "))
+area=3.14*r*r
+print("Area of Circle is: ", area)
---
+++ rectangleArea.py
@@ -0,0 +1,4 @@
+l=int(input("Length of rectangle: "))
+b=int(input("Breadth of rectangle: "))
+area=l*b
+print("Area of rectable is: ",area)
